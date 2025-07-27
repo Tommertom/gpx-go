4b02d85a3d5bbafdd2470c8a187fd2741a1cc602
Tommertom <Tom.Gruintjes@gmail.com>
2025-07-27 22:59:10 +0200

    test

---
+++ dev.bat
@@ -0,0 +1,8 @@
+@echo off
+echo 🔧 Starting local development server...
+echo 📱 GPX Go! will be available at: http://localhost:8000
+echo 🌐 Network access at: http://[your-ip]:8000
+echo.
+echo Press Ctrl+C to stop the server
+echo.
+python -m http.server 8000
